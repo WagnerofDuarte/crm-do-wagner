738edf639ad40ad89172e190db63cca801122a46
WagnerofDuarte <wagnerofduarte@gmail.com>
2020-06-23 21:55:39 -0300

    commit 3

--- crm/projetos/view.py
+++ crm/projetos/view.py
@@ -5,13 +5,26 @@ import os
 
 projeto = Blueprint('projeto', __name__, template_folder="templates")
 
+@projeto.route('/', methods=['POST', 'GET'])
+def index():
+
+    projetos = Projeto.query.all()
+    
+    if(request.method == 'POST'):
+        nome = request.form['pesquisa']
+        search = "%{}%".format(nome)
+        projetos = Projeto.query.filter(Projeto.nome.like(search)).all()
+        for i in projetos:
+            print(i.nome)
+
+    return render_template('teste.html', projetos = projetos)
+
 @projeto.route('/especifico/<_id>', methods=['GET'])
 def especifico(_id):
 
     i = 0
 
     projeto = Projeto.query.get_or_404(_id)
-
     projeto_comentarios = projeto.comentarios.split('|')
     
     return render_template('especifico_projetos.html', projeto = projeto, projeto_comentarios = projeto_comentarios)
